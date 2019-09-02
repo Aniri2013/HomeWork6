7132f0c30a59fac4b17fba1450396eaaf0fda439
Aniri2013 <aniri-13.rodina@yandex.ru>
2019-09-02 23:04:10 +0300

    homework

---
+++ Task1.py
@@ -0,0 +1,44 @@
+school = {
+    '1a': 15,
+    '1b': 20,
+    '2c': 25,
+    '6a': 30,
+    '7b': 35
+}
+
+if __name__ == '__main__':
+    while True:
+        print('Вывести количество учеников - введи ОБЩ - '
+              '\nИзменить количество учеников в классе - введи ИЗМ - '
+              '\nСоздать новый класс - введи НК - '
+              '\nУдалить класс - введи УДАЛИТЬ - '
+              '\nПосчитать общее количество учеников - введи СЧЁТ'
+              '\nЗавершить программу - введи ВЫХ')
+
+        deystvie = input('\nВведи команду - ')
+        if deystvie == 'ОБЩ':
+            print(school.items())
+            print('\n')
+
+        elif deystvie == 'ИЗМ' or deystvie == 'НК':
+            nomer_classa = input('\n Введи класс который нужно изменить или создать - ')
+            kolichestvo = input('\n Введи количество учеников для класса - ')
+            school[nomer_classa] = kolichestvo
+
+        elif deystvie == 'УДАЛИТЬ':
+            nomer_classa = input('\n Введи класс который нужно удалить - ')
+            print(nomer_classa)
+            del school[nomer_classa]
+
+        elif deystvie == 'CЧЕТ':
+            sum = 0
+            for key in school:
+                sum += school[key]
+
+            print(sum)
+            print('\n')
+
+        elif deystvie == 'ВЫХ':
+            break
+        else:
+            print('Команда не найдена, попробуйте снова\n')
